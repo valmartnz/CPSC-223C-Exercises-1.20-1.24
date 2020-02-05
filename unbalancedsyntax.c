69f28f67d2a15f1637500dfebb64dbd550424f5c
valmartnz <47126912+valmartnz@users.noreply.github.com>
2020-02-05 14:42:01 -0800

    Update unbalancedsyntax.c

--- unbalancedsyntax.c
+++ unbalancedsyntax.c
@@ -10,8 +10,10 @@ int main(int argc, const char* argv[]) {
   bool ok = openfiles(argc, argv, &fin, &fout, &n, "Usage: ./unbalancedsyntax inputfile outputfile n");
   if(!ok) { return 1; }
 
-  int p, b, cb;
+  int c, p, b, cb;
   p = b = cb = 0;
+  int dq, sq, comment;
+  dq = sq = comment = 1;
 
   // reads input file to check if syntax is balanced
   while((c = fgetc(fin)) != EOF) {
@@ -21,19 +23,39 @@ int main(int argc, const char* argv[]) {
       b++;
     if(c == '{')
       cb++;
+    if(c == '"')
+      dq *= -1;
+    if(c == '\'')
+      sq *= -1;
+    if(c == '/') {
+      if((c = fgetc(fin)) == '*'){
+        comment *= -1;
+      }
+    }
+    if(c == '*') {
+      if((c = fgetc(fin)) == '/'){
+        comment *= -1;
+      }
+    }
     if(c == ')')
       p--;
     if(c == ']')
       b--;
     if(c == '}')
-      cb--:
+      cb--;
   }
-  if(p > 0 || p < 0)
-    printf("Unbalanced paranthesis\n");
-  if(b > 0 || b < 0)
-    printf("Unbalanced bracket\n");
-  if(cb > 0; || cb < 0)
-    printf("Unbalanced curly brace\n");
+  if(p != 0)
+    printf("Unmatched paranthesis\n");
+  if(b != 0)
+    printf("Unmatched bracket\n");
+  if(cb != 0)
+    printf("Unmatched curly brace\n");
+  if(dq != 1)
+    printf("Unmatched double quote\n");
+  if(sq != 1)
+    printf("Unmatched single quote\n");
+  if(comment != 1)
+    printf("Unmatched comment\n");
 
   closefiles(fin, fout);
   return 0;

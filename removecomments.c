4f53733e974e473cb36726b305b6a966cbffe3ce
valmartnz <47126912+valmartnz@users.noreply.github.com>
2020-02-05 12:54:12 -0800

    Update removecomments.c

--- removecomments.c
+++ removecomments.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include "files.h"
 
+int main(int argc, const char* argv[]) {
+  FILE* fin;
+  FILE* fout;
+  int n;
+
+  bool ok = openfiles(argc, argv, &fin, &fout, &tab_interval, "Usage: ./detab inputfile outputfile n");
+  if(!ok) { return 1; }
+
+  int c, nc, position;
+  nb = position = 0;
+
+  // reads input file to remove comments
+  while((c = fgetc(fin)) != EOF) {
+    if(c == '/') {
+      if((nc = fgetc(fin)) == '*') {
+        while(c != '*' || nc != '/') {
+          c = nc;
+          nc = fgetc(fin);
+        }
+      } else if((nc = fgetc(fin)) == '/') {
+        while(c != '*' || nc != '/') {
+          c = nc;
+          nc = fgetc(fin);
+        }
+    } // 
+  }
+
+  closefiles(fin, fout);
+  return 0;
+}
